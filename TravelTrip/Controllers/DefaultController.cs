2cf3e1379f96a0118268e1daf2368b2bfe5f9098
pc <pc@DESKTOP-A2G4J05>
2024-02-19 20:43:09 +0300

    Add project files.

---
+++ TravelTrip/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Controllers;
+using TravelTrip.Models.Sınıflar;
+
+namespace TravelTrip.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        } 
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
